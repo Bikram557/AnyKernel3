d418c058f01f0bbecd61b4271c9c977923ee5755
Bikram Mukherjee <kitkatmukherjee2015@gmail.com>
2020-09-04 10:33:18 +0600

    change Kernel Name

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=SolarisKernel by Bikram557 @ GitHub
+kernel.string=StormBreaker Kernel by Bikram557 @ GitHub
 do.devicecheck=1
 do.modules=0
 do.systemless=1
